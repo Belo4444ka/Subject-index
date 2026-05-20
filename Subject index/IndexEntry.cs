9aa7f5e1be4db9d86349f244931f7f0cc50af8c3
Илья Петенко <ilyapetenko92@gmail.com>
2026-05-20 23:43:48 +0300

    Create new main class
    создание класса для хранения данных

---
+++ Subject index/IndexEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subject_index
+{
+    public class IndexEntry
+    {
+        public string Word { get; set; }
+        public List<int> pages { get; set; }
+
+        public IndexEntry(string Name) {
+            Word = Name;
+            pages = new List<int>();
+        }
+
+        //метод добавления страницы
+        public void AddPage(int page)
+        {
+            if (!pages.Contains(page))
+            { 
+                pages.Add(page);
+            }
+        }
+        //метод удаление страницы
+        public bool RemovePage(int page) 
+        {
+            return pages.Remove(page);
+        }
+    }
+}
